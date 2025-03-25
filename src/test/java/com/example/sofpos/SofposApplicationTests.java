b2960c21ffbcac3123aa7316e2381d2cad25758c
mohito <mypywelcome@gmail.com>
2025-03-25 19:37:01 +0400

    added_pos-system-app

---
+++ src/test/java/com/example/sofpos/SofposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sofpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
